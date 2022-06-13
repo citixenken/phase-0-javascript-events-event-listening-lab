3cc4035a9178284324665fbfe3e44c58f309e9a3
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-13 16:36:51 +0300

    commit tree b5f0fd549623e6ee2c90b67c6516d70ed343c7b4

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+  const input = document.getElementById("input");
+
+  //   function clickPrompt() {
+  //     alert("I've been clicked!");
+  //   }
+  //   input.addEventListener("click", clickPrompt);
+
+  //shorthand
+  input.addEventListener("click", function () {
+    alert("I have been clicked!");
+  });
 }
+
+//activating the eventListener
+addingEventListener();
